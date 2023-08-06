7e9f4509bb136769439fedc6754663a3d7d1fca9
hamitLicina <hamitlicina@gmail.com>
2023-08-07 00:03:00 +0300

    Contact, Form and List folders and files added

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import './App.css'
+import Contacts from './components/Contacts';
+
+function App() {
+
+  return (
+    <>
+    <div className='app-container'>
+      < Contacts />
+    </div>
+    </>
+  )
+}
+
+export default App

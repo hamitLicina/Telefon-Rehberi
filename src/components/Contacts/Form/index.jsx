8e3e0da6a2dcdf1ae51f39c842ee479eee8ebd9d
hamitLicina <hamitlicina@gmail.com>
2023-08-09 10:47:20 +0300

    Rehbere filtreleme özellikleri eklendi, Kayıtların listelenmesi işlemleri tamamlandı

--- src/components/Contacts/Form/index.jsx
+++ src/components/Contacts/Form/index.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import {useState} from 'react'
 
-function Form() {
+function Form({setContacts, contacts }) {
+
+  const [form, setForm] = useState({fullname: "", phone_number: ""});
+
+  const onChangeInput = (e) => {
+    setForm({...form, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (form.fullname === '' || form.phone_number === '') {
+      return false;
+    }
+    setContacts([...contacts, form])
+
+    setForm({ fullname: '', phone_number: ''});
+    
+  }
+
   return (
-    <div>Form</div>
-  )
+    <form onSubmit={onSubmit}>
+      <div>
+        <input name='fullname' type="text" placeholder='Full Name' value={form.fullname} onChange={onChangeInput} />
+      </div>
+      <div>
+        <input name='phone_number' type="text" placeholder='Phone Number' value={form.phone_number} onChange={onChangeInput} />
+      </div>
+      <div>
+        <button onClick={onSubmit}>Add</button>
+      </div>
+    </form>
+  );
 }
 
 export default Form
--- src/components/Contacts/index.jsx
+++ src/components/Contacts/index.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import List from './List'
 import Form from './Form'
 
 function Contacts() {
+  const [contacts, setContacts] = useState([
+    {
+      fullname: 'Hamit',
+      phone_number: '0555'
+    },
+    {
+      fullname: 'Kerim',
+      phone_number: '0111'
+    },
+    {
+      fullname: 'Kemal',
+      phone_number: '02222'
+    }
+  ]);
+
+  useEffect(() => {
+    console.log(contacts);
+  }, [contacts])
   return (
     <div>
-        < List />
-        < Form />
+        < List contacts={contacts} />
+        < Form setContacts={setContacts} contacts={contacts} />
     </div>
   )
 }
